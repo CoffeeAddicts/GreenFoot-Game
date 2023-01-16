0d7cf533bd77deada7ab323f75d1cfac679d0e39
JuliaWozniak100153025 <114222286+JuliaWozniak100153025@users.noreply.github.com>
2023-01-16 14:48:25 +0000

    Added game over screen

---
+++ Greenfoot Game/GameOverScreen.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GameOverScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GameOverScreen extends World
+{
+
+    /**
+     * Constructor for objects of class GameOverScreen.
+     * 
+     */
+    public GameOverScreen()
+    {    
+    public static final int WIDE = 800;
+    public static final int HIGH = 600;
+    public static final int CELL = 1;
+    }
+    
+    public GameOverScreen()
+    {   
+    super(WIDE, HIGH, CELL, false); 
+    }
+}
