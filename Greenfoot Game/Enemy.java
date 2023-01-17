b35e01375e8c010c8b26747496ce67ec972a4d8c
JuliaWozniak100153025 <114222286+JuliaWozniak100153025@users.noreply.github.com>
2023-01-17 10:24:08 +0000

    CodeUpdate
    Attempt to make enemy run around - wip

--- Greenfoot Game/Enemy.java
+++ Greenfoot Game/Enemy.java
@@ -8,12 +8,18 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Enemy extends Actor
 {
-    /**
-     * Act - do whatever the Enemy wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act()
     {
-        // Add your action code here.
+    move(1);
+    if (Greenfoot.getRandomNumber(10)<1)
+    {
+            turn(Greenfoot.getRandomNumber(90)-90);
+    }
+    
+    if (isAtEdge())
+    {
+            turn(180);
+    }
     }
 }
+
