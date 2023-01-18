817e499aba06c00a16141e537d3d1720c04606ed
JuliaWozniak100153025 <114222286+JuliaWozniak100153025@users.noreply.github.com>
2023-01-18 14:11:48 +0000

    Added lvl2
    Added 2nd level.

---
+++ Greenfoot Game/Level_Two.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Level_Two here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Level_Two extends World
+{
+
+    /**
+     * Constructor for objects of class Level_Two.
+     * 
+     */
+    public Level_Two()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1000, 800, 1); 
+    }
+}
