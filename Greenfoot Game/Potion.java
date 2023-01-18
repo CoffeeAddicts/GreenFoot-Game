f47d74ae9d7f7ecedbdab8e33a4a067c65159012
JuliaWozniak100153025 <114222286+JuliaWozniak100153025@users.noreply.github.com>
2023-01-18 15:23:02 +0000

    GraphicsChange
    Added new graphics, clock and potion.

---
+++ Greenfoot Game/Potion.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Potion here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Potion extends Actor
+{
+    /**
+     * Act - do whatever the Potion wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
